3a089133ca2ac9958fabf0dca6d8b21d7945cf26
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-07 17:19:28 +0300

    Final

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,40 +1,42 @@
+import { getProviders, signIn as SignIntoProvider } from "next-auth/react";
+import Header from "../../components/Header";
 
-import {getProviders, signIn as SignIntiProvider} from 'next-auth/react'
-import Header from '../../components/Header';
-
-function signIn({providers}) {
+function signIn({ providers }) {
   return (
-    <div className='bg-gray-50 overflow-y-scroll scrollbar-hide'>
-    <Header/>
-    <div className='flex flex-col items-center justify-center min-h-screen mt-4 py-14 text-center '>
-      <div className=' font-xs italic'>
-        <img src="https://links.papareact.com/ocw" alt=""  className='w-80'/>
-        <p>This is not a real app built for education purpose</p>
-      </div>
-      <div className='items-center'>
-      
-        {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button className='p-3 mt-40  bg-blue-500 text-white rounded-lg' onClick={() => SignIntiProvider(provider.id, {callbackUrl:'/'})}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
-    
-      
-    </div>
-  )
-}
-export async function getServerSideProps(){
-    const providers= await getProviders();
+    <>
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-55 px-14 text-center">
+        <img className="w-80" src="http://links.papareact.com/ocw" alt="" />
+        <p className="font-xs italic">
+          This is not a REAL app, it is built for educational purposes only
+        </p>
 
-    return{
-        props:{
-            providers
-        },
-    }
+        <div className="mt-40">
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name}>
+              <button
+                className="p-3 bg-blue-500 rounded-lg text-white"
+                onClick={() =>
+                  SignIntoProvider(provider.id, { callbackUrl: "/" })
+                }
+              >
+                Sign in with {provider.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default signIn
+export async function getServerSideProps() {
+  const providers = await getProviders();
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+
+export default signIn;
