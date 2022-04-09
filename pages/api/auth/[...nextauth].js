5e08b5e2512907e91c0856df1fa7b414c9ad608a
LeeroyKiplagat <leeroykiplagat@gmail.com>
2022-04-10 01:08:45 +0300

    FINAL

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,7 +11,7 @@ export default NextAuth({
     // ...add more providers here
   ],
   pages:{
-    signIn:"/auth/signin",
+    signIn:"/api/auth/signin",
   },
   callbacks:{
     async session({session,token,user}){
